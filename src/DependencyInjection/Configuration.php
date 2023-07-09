695ca5e349abe19cd4f002c6b9f98ba2af525c81
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-07-09 21:38:40 +0300

    9

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -7,6 +7,22 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
+    protected const DEFAULT_DIRECTIONS_REQUEST = [
+        "profile" => "driving",
+        "geometries" => "geojson",
+        "steps" => true,
+        "alternatives" => true,
+        "exclude" => [
+            "points" => [],
+            "motorway" => false,
+            "toll" => false,
+            "ferry" => false,
+            "unpaved" => false,
+            "cash_only_tolls" => false,
+            ]
+    ];
+
+
     /**
      * {@inheritdoc}
      */
@@ -66,56 +82,11 @@ class Configuration implements ConfigurationInterface
 
 
                             ->end()
+                        ->defaultValue(self::DEFAULT_DIRECTIONS_REQUEST )
                         ->end()
                     ->end()
                 ->end()
 
-
-                /*->arrayNode("request")
-                    ->children()
-                        # Вид маршрута
-                        ->enumNode("profile")
-                            ->values(["driving","driving-traffic","walking","cycling"])
-                            ->defaultValue("driving")
-                        ->end()
-
-                        # Тип возвращаемой геометрии
-                        ->enumNode("geometries")
-                            ->values([ "geojson", "polyline", "polyline6" ])
-                            ->defaultValue("geojson")
-                        ->end()
-
-
-                        # Указывает нужно ли возвращать шаги маршрута
-                        ->booleanNode("steps")->defaultValue(true)->end()
-
-                        # Нужно ли искать более одного маршрута
-                        ->booleanNode("alternatives")->defaultValue(true)->end()
-
-                        # Список дорог и точек который необходимо исключить из маршрута
-                        ->arrayNode("exclude")
-                            ->children()
-                                ->booleanNode('motorway')->defaultFalse()->end()
-                                ->booleanNode('toll')->defaultFalse()->end()
-                                ->booleanNode('ferry')->defaultFalse()->end()
-                                ->booleanNode('unpaved')->defaultFalse()->end()
-                                ->booleanNode('cash_only_tolls')->defaultFalse()->end()
-                                ->arrayNode('points')
-                                    ->arrayPrototype()
-                                        ->children()
-                                            ->floatNode("lat")->min(-90.0)->max(90.0)->end()
-                                            ->floatNode("long")->min(-180.0)->max(180.0)->end()
-                                        ->end()
-                                    ->end()
-                                ->end()
-                            ->end()
-                            ->defaultValue([])
-                        ->end()
-
-                    ->end()
-
-                    ->end()
-                ->end()*/
             ->end()
         ->end();
 
